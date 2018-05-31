75d969c2e9a68874960fc48de2f43aab15077a2a
noppelmax <noppelmax@googlemail.com>
2018-05-31 22:21:20 +0000

    reload hidden

--- spaceinfo.sh
+++ spaceinfo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+git pull origin master
 
 #feh --file list.txt  -F -D 1 -z -Z
 while [ 1 ]; do
-git pull origin master
 sudo fbi -T 1 -t 5 *.jpg -a -noverbose -u
 sleep 10
+git pull origin master
 sudo killall fbi
 done
