6e4734b3540cd3de749ddfc5ca0136dc78519c8b
noppelmax <gitkraken@noppelmax.online>
2018-06-01 17:52:36 +0200

    move images to folder

--- spaceinfo.sh
+++ spaceinfo.sh
@@ -5,7 +5,7 @@ git pull origin master
 
 #feh --file list.txt  -F -D 1 -z -Z
 while [ 1 ]; do
-sudo fbi -T 1 -t 7 /opt/spaceinfo/*.jpg -a -noverbose -u
+sudo fbi -T 1 -t 7 /opt/spaceinfo/imgs/*.jpg -a -noverbose -u
 sleep 1800
 git pull origin master
 sudo killall fbi
