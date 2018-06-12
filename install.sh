741b722711b8835e2c893748fa4ab4c46bf74e9b
HACKER-3000 <hacker3000@posteo.org>
2018-06-12 13:39:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,11 @@ cp spaceinfo.service //lib/systemd/system/spaceinfo.service
 systemctl daemon-reload
 
 chmod +x spaceinfo.sh
+
+chmod +x musicinstall.sh
+
+./musicinstall.sh
+
+echo "Starting Spaceinfo Script..."
+
+./spaceinfo.sh
