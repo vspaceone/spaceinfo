96b5356f45d82badb76592ae2d954baa546231a2
HACKER-3000 <hacker3000@posteo.org>
2018-06-12 13:41:30 +0200

    Update musicinstall.sh

--- musicinstall.sh
+++ musicinstall.sh
@@ -10,8 +10,7 @@ sudo chmod a+x shairport
 sudo update-rc.d shairport defaults
 
 
-sudo cat > /etc/apt/sources.list.d/upmpdcli.list "deb http://www.lesbonscomptes.com/upmpdcli/downloads/raspbian/ stretch main deb-src
-http://www.lesbonscomptes.com/upmpdcli/downloads/raspbian/ stretch main"
+sudo cat > /etc/apt/sources.list.d/upmpdcli.list $"deb http://www.lesbonscomptes.com/upmpdcli/downloads/raspbian/ stretch main\ndeb-src http://www.lesbonscomptes.com/upmpdcli/downloads/raspbian/ stretch main"
 sudo apt install dirmngr
 gpg --keyserver pool.sks-keyservers.net --recv-key F8E3347256922A8AE767605B7808CE96D38B9201
 gpg --export '7808CE96D38B9201' | sudo apt-key add -
