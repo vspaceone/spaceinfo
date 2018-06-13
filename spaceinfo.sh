06fd6f381c3fcd171a4c00c6240e0763615bbc9c
noppelmax <gitkraken@noppelmax.online>
2018-06-13 10:42:32 +0200

    increase time

--- spaceinfo.sh
+++ spaceinfo.sh
@@ -8,7 +8,7 @@ git pull origin master
 #feh --file list.txt  -F -D 1 -z -Z
 while [ 1 ]; do
 	echo "New loop started"
-	sudo fbi -T 1 -t 10 /opt/spaceinfo/imgs/*.jpg -a -noverbose -u
+	sudo fbi -T 1 -t 14 /opt/spaceinfo/imgs/*.jpg -a -noverbose -u
 	sleep 1800
 	git pull origin master
 	sudo killall fbi
