3fbf114920292deb031a6a5a1d9fbea7cf09932c
noppelmax <gitkraken@noppelmax.online>
2018-10-11 15:56:23 +0200

    serve diretory via http

---
+++ server.py
@@ -0,0 +1,129 @@
+#!/bin/python
+
+import os
+import json
+import configparser
+import sys
+import logging
+
+import SimpleHTTPServer
+import SocketServer
+import signal
+
+# Constants
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_PATCH = 0
+VERSION = "v"+str(VERSION_MAJOR)+"."+str(VERSION_MINOR)+"."+str(VERSION_PATCH)
+
+
+PORT = 8080
+IP = "0.0.0.0"
+
+# Setting up logger
+logger = logging.getLogger('server.py')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+def sigterm_handler(_signo,_stack_frame):
+	logger.info("SIGTERM received. Cleaning up...")
+	sys.exit(0)
+
+def sigint_handler(_signo, _stack_frame):
+	logger.info("SIGINT received. Cleaning up...")
+	sys.exit(0)
+
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_POST(s):
+		s.send_response(200)
+		s.send_header("Content-type","application/json")
+		s.end_headers()
+	def do_GET(s):
+		s.send_response(200)
+		s.send_header("Content-type","application/json; charset=utf-8")
+		s.end_headers()
+		s.wfile.write(generateDirectory("internal"))
+
+def printUsage():
+	print("Usage: server.py <slideshow> <servername>")
+	print("slideshow:  Adds only pages where this slideshow is")
+	print("            noted in the configfile.")
+	print("servername: Name the server spaceinfo is running on,")
+	print("            so the local pages could be prefixed.")
+	print("            Don't forget http(s)://")
+
+def main():
+	httpd = SocketServer.TCPServer((IP,PORT), ServerHandler)
+
+	try:
+		httpd.serve_forever()
+	finally:
+		httpd.server_close()
+		logger.info("Server killed")
+
+
+def generateDirectory(slideshow):
+	directory = []
+
+
+	servername = "http://spaceinfo.noppelmax.online/"
+	logger.info("Generating directory for slideshow %s" % slideshow)
+	DEFAULT_TIMEOUT = 10
+	p = os.path.relpath("pages")
+
+	for d in os.listdir(p):
+		if not os.path.isfile(os.path.join("pages",d)):
+			link = ""
+			timeout = DEFAULT_TIMEOUT
+			slideshows = []
+			for f in os.listdir(os.path.join("pages",d)):
+				if f == "config.ini":
+					config = configparser.ConfigParser()
+					config.read(os.path.join("pages",d,f))
+					try:
+						link = config["Page-Settings"]["external_link"]
+					except KeyError:
+						pass
+					try:
+						timeout = config["Page-Settings"]["timeout"]
+					except KeyError:
+						pass
+					try:
+						slideshows =  config["Page-Settings"]["slideshows"].split(" ")
+					except KeyError:
+						pass
+				if f == "index.html":
+					link = os.path.join(servername,"pages",d,f)
+				else:
+					pass
+
+			# Checking for Errors
+			if link == "":
+				logger.error(os.path.join("pages",d)+" got no index.html neither config.cfg")
+				continue
+			if slideshow not in slideshows:
+				continue
+			directory.append({ "link": link, "timeout": timeout })
+
+	ret = (json.dumps(directory, indent=2, sort_keys=True))
+	return ret
+
+if __name__ == '__main__':
+	logger.info("Running version "+VERSION)
+	logger.info("Starting server...")
+	signal.signal(signal.SIGTERM, sigterm_handler)
+	signal.signal(signal.SIGINT, sigint_handler)
+	main()
+else:
+	logger.error("Run this script as main pls!")
+
+
+
